00dc6a161a06a64c822801b3d94df1dba53cacf8
skiq <gl@skiq.pl>
2025-03-31 23:13:18 +0200

    brakeman fix

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+  def safe_redirect_url(post)
+    uri = URI.parse(post.redirect_url)
+    uri.scheme.in?(%w[http https]) ? post.redirect_url : "#"
+  rescue URI::InvalidURIError
+    "#"
+  end
 end
