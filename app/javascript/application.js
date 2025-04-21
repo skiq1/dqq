cdb8a45a1e9dd4ac9c5a0f2c44c5dfa9297cf8ce
skiq <gl@skiq.pl>
2025-04-21 14:48:24 +0200

    buttons, pagy and search fix & layout update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo"
 import "@hotwired/turbo-rails"
+
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+import Rails from "@rails/ujs"
+Rails.start()
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-300.times do
-  Post.create(
-    title: Faker::Lorem.sentence(word_count: 3),
-    description: Faker::Lorem.paragraph(sentence_count: 5),
-    user_id: 1,
-    created_at: Faker::Date.between(from: 1.year.ago, to: Date.today),
-    updated_at: Faker::Date.between(from: 1.year.ago, to: Date.today)
-  )
+start_date = 2.months.ago.to_date
+end_date = Date.today
+
+(start_date..end_date).each do |date|
+  num_posts = rand(0..5)
+  # Start from the beginning of the day
+  current_time = date.beginning_of_day + rand(0..3600) # add up to an hour randomly at start
+
+  num_posts.times do
+    # Add a random gap (e.g., 10 to 120 minutes) to keep things in order
+    time_gap = rand(10..120).minutes
+    current_time += time_gap
+
+    Post.create(
+      title: Faker::Lorem.sentence(word_count: 3),
+      description: Faker::Lorem.paragraph(sentence_count: 5),
+      user_id: 1,
+      created_at: current_time,
+      updated_at: current_time
+    )
+  end
 end
